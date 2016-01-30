b5dca76cf15bf18e5ed48cd9234c65695d14242f
Billie <billielorraine@users.noreply.github.com>
2016-01-30 02:38:39 -0600

    Shopping List Exercise

---
+++ 01 Front End Development Certification/Basic Javascript/Shopping List.js
@@ -0,0 +1,9 @@
+/* Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub-arrays in the list. */
+
+var myList = [["milk",1], ["bag of chocolate chip cookies",3], ["bag of bagels",1], ["vegetable cream cheese",1], ["eggs",1], ["bag of Hot Cheetos",2]];
